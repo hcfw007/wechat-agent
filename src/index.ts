66330bc4a9ff655d2c92f3d1d056a5f2c87572dd
NickWang <brotherstyx@gmail.com>
2021-09-03 12:08:53 +0800

    feat: 🎸 terminal qrcode

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { Wechaty } from "wechaty"
+import { Wechaty, ScanStatus } from "wechaty"
+import { QRCodeTerminal } from 'wechaty-plugin-contrib'
+
+const config = {
+  small: true,   // default: false - the size of the printed QR Code in terminal
+}
 
 async function main() {
   const bot = new Wechaty()
+  bot.use(QRCodeTerminal(config))
   bot
-    .on("scan", (qrcode, status) =>
-      console.log(
-        `Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(
-          qrcode
-        )}`
-      )
-    )
+    .on("scan", (qrcode, status) => {
+      console.log('Scan QR code to login')
+    })
     .on("login", (user) => console.log(`User ${user} logged in`))
     .on("message", (message) => console.log(`Message: ${message}`))
   await bot.start()
