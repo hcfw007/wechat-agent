a83832fbf5c375a49b8aa147e3f20e46dfa4da05
NickWang <brotherstyx@gmail.com>
2021-09-26 21:19:12 +0800

    feat: 🎸 info

---
+++ src/controls/actions/info.action.ts
@@ -0,0 +1,10 @@
+import { Global } from '@src/utils/data.interface'
+import { log, Message } from 'wechaty'
+
+const PRE = 'info'
+
+export const info = async (g: Global, message: Message, commandObject: any) => {
+  const infoStr = `当前微信名：${g.bot.userSelf().name()}\n操作微信名：${g.commander.name()}\n转发微信名：${g.target? g.target.name() : '无'}`
+  log.info(PRE, `reporting bot info: ${ infoStr.replace(/\n/g, '') }`)
+  g.commander.say(infoStr)
+}
