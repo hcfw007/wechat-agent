532ac2bd3fc7dbe5482b2c01ca8a7222b11466fe
NickWang <brotherstyx@gmail.com>
2021-09-28 12:55:01 +0800

    fix: 🐛 info formmat

--- src/controls/actions/info.action.ts
+++ src/controls/actions/info.action.ts
@@ -4,12 +4,13 @@ import { log } from 'wechaty'
 const PRE = 'info'
 
 export const info: Action = async (g, message, commandObject) => {
-  const infoStr = `
-当前微信名：${g.bot.userSelf().name()}
+  
+  const infoStr = 
+`当前微信名：${g.bot.userSelf().name()}
 操作微信名：${g.commander.name()}
 转发微信名：${g.target? g.target.name() : '无'}
-群白名单: ${ g.roomNameList.length > 0 ? g.roomNameList.join(', ') : '无' }
-`
+群白名单: ${ g.roomNameList.length > 0 ? g.roomNameList.join(', ') : '无' }`
+
   log.info(PRE, `reporting bot info: ${ infoStr.replace(/\n/g, ';') }`)
   g.commander.say(infoStr)
 } 
