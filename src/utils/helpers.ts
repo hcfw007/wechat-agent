6976c1a13a53a0af8ad20c78e52453a7ea21689c
NickWang <brotherstyx@gmail.com>
2021-10-18 12:31:35 +0800

    fix: 🐛 retry once if cannot find commander

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -14,3 +14,9 @@ export function parsePayload(payload?: string[]): any {
 export async function getRoomNameList(rooms: Array<Room>): Promise<string[]> {
   return await Promise.all(rooms.map(async room => await room.topic()))
 }
+
+export function sleep(seconds: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000)
+  })
+}
