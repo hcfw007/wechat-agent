e617054dbda0671ce73f87c4a4351064d7095ae0
NickWang <brotherstyx@gmail.com>
2021-10-18 20:31:36 +0800

    feat: 🎸 help function

--- src/controls/actions.ts
+++ src/controls/actions.ts
@@ -2,10 +2,27 @@ import { kick } from './actions/kick.action'
 import { info } from './actions/info.action'
 import { say } from './actions/say.action'
 import { roomManage } from './actions/roomManage.action'
+import { Action } from '@src/utils/data.interface'
+
+const actionDescription = {
+  kick: '踢掉$room中的$target',
+  info: '获取mouthpiece基本信息',
+  roomManage: '根据$roomCommand进行群聊管理，目前支持add，将$target的群加入群聊白名单',
+  say: '在$in群聊或者对$to说话，内容是$content，单聊优先'
+}
+
+const help: Action = async (g) => {
+  let result = ''
+  for (const action in actions) {
+    result += `${action}: ${actionDescription[action]} \n`
+  }
+  await g.commander.say(result)
+}
 
 export const actions = {
   kick,
   info,
   say,
   roomManage,
+  help,
 }
