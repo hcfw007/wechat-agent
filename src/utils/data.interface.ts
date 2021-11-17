2d0fba7770ab461e504d41dab972d5dfd6fda235
NickWang <brotherstyx@gmail.com>
2021-11-17 15:10:36 +0800

    feat: 🎸 add stat class

--- src/utils/data.interface.ts
+++ src/utils/data.interface.ts
@@ -11,6 +11,29 @@ export interface Global {
   roomNameList: Array<string>,
 }
 
+export class stat {
+  messageReceived: number
+  messageForwarded: number
+  commandAccepted: number
+  messageSend: number
+
+  constructor() {
+    this.messageReceived = 0
+    this.messageForwarded = 0
+    this.commandAccepted = 0
+    this.messageSend = 0
+  }
+
+  toOutputString(): string {
+    const result =
+`收到消息数：${ this.messageReceived }
+转发消息数：${ this.messageForwarded }
+收到命令数：${ this.commandAccepted }
+发送消息数: ${ this.messageSend }`
+    return result
+  }
+}
+
 export interface CommandObject {
   command: string,
   [property: string]: string
