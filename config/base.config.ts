82291ed20d0e25a7ac1749e4d6c36702e94fd523
NickWang <brotherstyx@gmail.com>
2022-01-18 20:43:56 +0800

    feat: 🎸 move some config to json file

--- config/base.config.ts
+++ config/base.config.ts
@@ -2,6 +2,4 @@ export default {
   maxLoginTime: 300000, // max qrcode waiting time
   targetContactName: 'MyOnlyStar',
   commanderContactName: 'MyOnlyStar',
-  allowedRooms: [ '厦大08级凌云一207室' ],
-  disallowedContacts: [],
 }
