6472805036975e50e6629eed542740ef56e0cf13
NickWang <brotherstyx@gmail.com>
2022-01-19 14:39:12 +0800

    feat: 🎸 report contact blacklist in info

--- src/controls/actions/info.action.ts
+++ src/controls/actions/info.action.ts
@@ -9,7 +9,9 @@ export const info: Action = async (g, message, commandObject) => {
 `当前微信名：${g.bot.userSelf().name()}
 操作微信名：${g.commander.name()}
 转发微信名：${g.target? g.target.name() : '无'}
-群白名单: ${ g.roomNameList.length > 0 ? g.roomNameList.join(', ') : '无' }`
+群白名单: ${ g.roomNameList.length > 0 ? g.roomNameList.join(', ') : '无' }
+联系人黑名单: ${ g.contactNameList.length > 0 ? g.contactNameList.join(', ') : '无' }
+`
 
   log.info(PRE, `reporting bot info: ${ infoStr.replace(/\n/g, ';') }`)
   await g.commander.say(infoStr)
